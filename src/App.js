72b31dc5b8ba58ae69d7de8f9787e156690457d0
Yener Yardim <yyardim@gmail.com>
2022-12-19 22:04:50 -0800

    components and else

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useContext } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import BooksContext from "./context/books";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { fetchBooks } = useContext(BooksContext);
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>Reading List</h1>
+            <BookList />
+            <BookCreate />
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from './context/books';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>
+        <Provider>
+            <App />
+        </Provider>        
+    </div>    
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
